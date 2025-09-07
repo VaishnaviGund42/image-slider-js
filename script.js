d17b78eb071ad97583d2a4cbaea87acc80474aad
VaishnaviGund42 <vaishnavigund42@gmail.com>
2025-09-07 14:32:59 +0530

    to add the body of next function

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const images = ["images/city-img.jpg", "images/city-near-lake.jpg", "images/palace.jpg", "images/night.jpg", "images/garden.jpg"];
+
+let index = 0;
+const cityImage = document.getElementById("cityImage");
 function next(){
-
-}
-
-function previous(){
+    index++;
+    if (index >= images.length) {
+        index = 0; // परत पहिल्या इमेजवर जा
+      }
+    cityImage.src = images[index];
+    }
     
-}
+
+
+
+
+
